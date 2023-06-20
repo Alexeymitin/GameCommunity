7b586bfbd442f513d3ec981c140aeefef4a78165
Alexey <adduction@mail.ru>
2023-06-20 21:28:49 +0300

    add comment list, commentCard, useInitialEffect

---
+++ src/entities/Comment/ui/CommentList/CommentList.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {
+	ThemeDecorator
+} from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { CommentList } from './CommentList';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+
+export default {
+	title: 'nameDirectory/CommentList',
+	component: CommentList,
+	argTypes: {
+		backgroundColor: { control: 'color' },
+	}
+} as ComponentMeta<typeof CommentList>;
+
+const Template: ComponentStory<typeof CommentList> = (args) => <CommentList {...args} />;
+
+export const Light = Template.bind({});
+Light.args = {};
+Light.decorators = [StoreDecorator({})];
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
---
+++ src/pages/ArticleDetailsPage/model/slices/articleDetailsCommentsSlice.ts
@@ -0,0 +1,51 @@
+import {
+	PayloadAction,
+	createEntityAdapter,
+	createSlice
+} from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { Comment } from 'entities/Comment';
+import { ArticleDetailsCommentsSchema } from '../types/ArticleDetailsCommentsSchema';
+import { fetchCommentByArticleId } from '../services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+  
+const commentsAdapter = createEntityAdapter<Comment>({
+	selectId: (comment) => comment.id,
+});
+
+export const getArticleComments = commentsAdapter.getSelectors<StateSchema>(
+	state => state.articleDetailsComments || commentsAdapter.getInitialState()
+);
+  
+const articleDetailsCommentsSlice = createSlice({
+	name: 'articleDetailsCommentsSlice.ts',
+	initialState: commentsAdapter.getInitialState<ArticleDetailsCommentsSchema>({
+		isLoading: false,
+		ids: [],
+		entities: {},
+		error: undefined
+	}),
+	reducers: {
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchCommentByArticleId.pending, (state) => {
+				state.error= undefined;
+				state.isLoading = true;
+			})
+			.addCase(
+				fetchCommentByArticleId.fulfilled, (
+					state, 
+					action: PayloadAction<Comment[]>
+				) => {
+					state.isLoading = false;
+					commentsAdapter.setAll(state, action.payload);
+				})
+			.addCase(fetchCommentByArticleId.rejected, (state, action) => {
+				state.isLoading = false;
+				state.error = action.payload;
+			});
+	}, 
+});
+
+export const {reducer: articleDetailsCommentsReducer} = articleDetailsCommentsSlice;
+  
