43a1ad7d12eee007544a741310992afe73a94c64
Alexey Mitin <adduction@mail.ru>
2023-11-30 18:06:14 +0300

    change preview storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,22 +1,22 @@
-import { addDecorator } from '@storybook/react';
-import { Theme } from '../../src/app/providers/ThemeProvider';
-import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
-import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
-import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
-import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { addDecorator } from "@storybook/react";
+import { Theme } from "../../src/app/providers/ThemeProvider";
+import { RouterDecorator } from "../../src/shared/config/storybook/RouterDecorator/RouterDecorator";
+import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
+import { SuspenseDecorator } from "../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator";
+import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator";
 
 export const parameters = {
-	actions: { argTypesRegex: '^on[A-Z].*' },
+	actions: { argTypesRegex: "^on[A-Z].*" },
 	controls: {
 		matchers: {
 			color: /(background|color)$/i,
 			date: /Date$/,
 		},
 	},
+	layout: "fullscreen",
 };
 
 addDecorator(StyleDecorator);
 addDecorator(ThemeDecorator(Theme.LIGHT));
 addDecorator(RouterDecorator);
 addDecorator(SuspenseDecorator);
-
