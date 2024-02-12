238665d3c56845db9a42790608510efa61eec5ac
Alexey Mitin <adduction@mail.ru>
2024-02-12 17:56:46 +0300

    change port for start

--- src/shared/ui/Popups/ui/Dropdown/Dropdown.tsx
+++ src/shared/ui/Popups/ui/Dropdown/Dropdown.tsx
@@ -5,43 +5,41 @@ import { DropdownDirection } from 'shared/types/ui';
 import { AppLink, AppLinkTheme } from '../../../AppLink/AppLink';
 import { Button, ButtonTheme } from '../../../Button/Button';
 import { mapDirectionClass } from '../styles/consts';
-import cls from './Dropdown.module.scss';
 import popupCls from '../styles/popup.module.scss';
+import cls from './Dropdown.module.scss';
 
 export interface DropdownItem {
-    disabled?: boolean;
-    content?: ReactNode;
-    onClick?: () => void;
-    href?: string;
+	disabled?: boolean;
+	content?: ReactNode;
+	onClick?: () => void;
+	href?: string;
 }
 
 interface DropdownProps {
-    className?: string;
-    items: DropdownItem[];
-    direction?: DropdownDirection;
-    trigger: ReactNode;
+	className?: string;
+	items: DropdownItem[];
+	direction?: DropdownDirection;
+	trigger: ReactNode;
 }
 
 export function Dropdown(props: DropdownProps) {
-	const {
-		className, trigger, items, direction = 'bottom right',
-	} = props;
+	const { className, trigger, items, direction = 'bottom right' } = props;
 
 	const menuClasses = [mapDirectionClass[direction]];
 
 	return (
 		<Menu as="div" className={classNames(cls.dropdown, {}, [className])}>
-			<Menu.Button className={popupCls.trigger}>
-				{trigger}
-			</Menu.Button>
+			<Menu.Button className={popupCls.trigger}>{trigger}</Menu.Button>
 			<Menu.Items className={classNames(cls.menu, {}, menuClasses)}>
 				{items.map((item, index) => {
-					const content = ({ active }: {active: boolean}) => (
+					const content = ({ active }: { active: boolean }) => (
 						<button
 							type="button"
 							disabled={item.disabled}
 							onClick={item.onClick}
-							className={classNames(cls.item, { [cls.active]: active })}
+							className={classNames(cls.item, {
+								[cls.active]: active,
+							})}
 						>
 							{item.content}
 						</button>
@@ -49,11 +47,11 @@ export function Dropdown(props: DropdownProps) {
 
 					if (item.href) {
 						return (
-							<Menu.Item 
-								as={AppLink} 
-								to={item.href} 
-								theme={AppLinkTheme.SECONDARY} 
-								disabled={item.disabled} 
+							<Menu.Item
+								as={AppLink}
+								to={item.href}
+								theme={AppLinkTheme.SECONDARY}
+								disabled={item.disabled}
 								key={`dropwdown-key-${index}`}
 							>
 								{content}
@@ -62,17 +60,16 @@ export function Dropdown(props: DropdownProps) {
 					}
 
 					return (
-						<Menu.Item 
-							as={Button} 
-							theme={ButtonTheme.CLEAR} 
-							disabled={item.disabled} 
+						<Menu.Item
+							as={Button}
+							theme={ButtonTheme.CLEAR}
+							disabled={item.disabled}
 							key={`dropwdown-key-${index}`}
 						>
 							{content}
 						</Menu.Item>
 					);
 				})}
-
 			</Menu.Items>
 		</Menu>
 	);
